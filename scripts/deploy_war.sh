e0297fd7330d418cde20015f52eb8e7a280b8d4f
yongdongan <ydan91@naver.com>
2025-11-06 16:00:25 +0900

    build t

--- scripts/deploy_war.sh
+++ scripts/deploy_war.sh
@@ -2,6 +2,6 @@
 #!/bin/bash
 set -e
 # appspec의 files 섹션이 ROOT.war 덮어쓰기 하므로 여기선 권한/정리만
-chown tomcat:tomcat /opt/tomcat/webapps/ROOT.war
+chown tomcat:tomcat /opt/tomcat/webapps/myapp.war
 # (선택) 이전 exploded 디렉터리 제거
-rm -rf /opt/tomcat/webapps/ROOT
+rm -rf /opt/tomcat/webapps/myapp
--- scripts/validate.sh
+++ scripts/validate.sh
@@ -2,4 +2,4 @@
 #!/bin/bash
 set -e
 # 로컬 헬스체크 (8080)
-curl -sf http://127.0.0.1:8080/ -o /dev/null
+curl -sf http://127.0.0.1:8080/myapp/hello -o /dev/null
